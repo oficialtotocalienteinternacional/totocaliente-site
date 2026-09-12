d0a386d3b74a335c0c0157c3249a359832f88b2e
TOTO CALIENTE IN <totocalienteinternacionalbooking@hotmail.com>
2026-09-12 02:17:15 +0200

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -4,7 +4,7 @@
   OneSignalDeferred.push(async function(OneSignal) {
     await OneSignal.init({
       appId: "26e80783-214b-4be1-9c3d-a932c01aaedf",
-      safari_web_id: "web.onesignal.auto.26e80783-214b-4be1-9c3d-a932c01aaedf",
+      safari_web_id: "web.onesignal.auto.147da6f8-95e8-4f3e-9e77-8dc52e1c58f0",
       notifyButton: {
         enable: true,
       },
